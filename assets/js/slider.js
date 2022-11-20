636554d74e2368ca0c6572e3ac99b9e21fe6f4a1
Koua Ekaza Wilfried <wilfried.koua@futurafric.com>
2022-11-20 15:45:51 +0000

    feat 🎷 add gallery feature 📷 add more themes 🎨 add more languages 🌐 add more features 🎁

---
+++ assets/js/slider.js
@@ -0,0 +1,96 @@
+$("#open-popup").magnificPopup({
+  items: [
+    {
+      src: "assets/img/works/nta1.png",
+      title: "NT'a Community",
+    },
+    {
+      src: "assets/img/works/nta2.png",
+      title: "NT'a Community",
+    },
+    {
+      src: "assets/img/works/nta_form.png",
+      title: "NT'a Community",
+    },
+    {
+      src: "assets/img/works/nta_dash.png",
+      title: "NT'a Community",
+    },
+    {
+      src: "assets/img/works/main_page.png",
+      title: "Easy cargo",
+    },
+    {
+      src: "assets/img/works/login_user.png",
+      title: "Easy cargo",
+    },
+    {
+      src: "assets/img/works/register_user.png",
+      title: "Easy cargo",
+    },
+    {
+      src: "assets/img/works/dashboard_page.png",
+      title: "Easy cargo",
+    },
+    {
+      src: "assets/img/works/cars.png",
+      title: "Easy cargo",
+    },
+    {
+      src: "assets/img/works/book_page.png",
+      title: "Easy cargo",
+    },
+    {
+      src: "assets/img/works/payment_page.png",
+      title: "Easy cargo",
+    },
+    {
+      src: "assets/img/works/lela.png",
+      title: "Lela group",
+    },
+    {
+      src: "assets/img/works/register.png",
+      title: "Lela group",
+    },
+    {
+      src: "assets/img/works/dash.png",
+      title: "Lela group",
+    },
+    {
+      src: "assets/img/works/transfer_sode.png",
+      title: "Lela group",
+    },
+    {
+      src: "assets/img/works/settings.png",
+      title: "Lela group",
+    },
+    {
+      src: "assets/img/works/loan.png",
+      title: "GTBANK loan simulator",
+    },
+    {
+      src: "assets/img/works/loan_1.png",
+      title: "GTBANK loan simulator",
+    },
+    {
+      src: "assets/img/works/toolbox.png",
+      title: "GTBANK Orange Toolbox",
+    },
+    {
+      src: "assets/img/works/menu.png",
+      title: "GTBANK Orange Toolbox",
+    },
+    {
+      src: "assets/img/works/basis.png",
+      title: "GTBANK Orange Toolbox",
+    },
+    {
+      src: "assets/img/works/transfer.png",
+      title: "GTBANK Orange Toolbox",
+    },
+  ],
+  gallery: {
+    enabled: true,
+  },
+  type: "image", // this is a default type
+});
